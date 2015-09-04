8ea69cffdd127a6bbe48a53dc05aa4198ff60da2
codingdefined <codingdefined@gmail.com>
2015-09-04 21:29:00 +0530

    Added Objects.js

---
+++ objects.js
@@ -0,0 +1,22 @@
+module.exports = {
+	nodeBook1 : {
+		bookName: "Professional Node.js",
+		author: "Pedro Teixeria",
+		buy: "http://www.amazon.in/Professional-Node-JS-Building-Javascript-Based-Scalable/dp/8126539283/ref=as_sl_pc_tf_mfw?&linkCode=wey&tag=codidefi-21"
+	},
+	nodeBook2 : {
+		bookName: "Node.Js In Action",
+		author: "Mike Cantelon, Marc Harter, T.J. Holowaychuk",
+		buy: "http://www.amazon.in/Node-Js-In-Action-Mike-Cantelon/dp/9351192601/ref=as_sl_pc_tf_mfw?&linkCode=wey&tag=codidefi-21"
+	},
+	nodeBook3 : {
+		bookName: "Angular JS",
+		author: "Brad Green",
+		buy: "http://www.amazon.in/Angular-JS-Brad-Green/dp/9351101266/ref=as_sl_pc_tf_mfw?&linkCode=wey&tag=codidefi-21"
+	},
+	nodeBook4 : {
+		bookName: "Professional ASP NET MVC",
+		author: "Jon Galloway",
+		buy: "http://www.amazon.in/Professional-ASP-NET-MVC-Jon-Galloway/dp/1118794753/ref=as_sl_pc_tf_mfw?&linkCode=wey&tag=codidefi-21"
+	}
+}
